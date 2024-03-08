09b42dfbcd536f6c919c498d832e0214b3e73140
emidolga <63514199+emidolga@users.noreply.github.com>
2024-03-08 12:55:14 -0300

    Add files via upload

---
+++ ChequesPropiosModule.cs
@@ -0,0 +1,40 @@
+﻿using Nancy;
+using Nancy.Security;
+using System;
+using System.Collections.Generic;
+using Vemn.Framework.ExceptionManagement;
+using Vemn.Framework.Logging;
+
+namespace HostCaldenONNancy.Modules
+{
+    public class ChequesPropiosModule : NancyModule
+    {
+        public ChequesPropiosModule() : base("api/ChequesPropios/")
+        {
+            Get<Models.ChequePropio[]>("GetEgresoChequesPropiosEntreFechas", p =>
+            {
+                this.RequiresAuthentication();
+                List<Models.ChequePropio> chequesPropiosLista = null;
+                try
+                {
+                    string desdeFecha = Request.Query["desdeFecha"];
+                    string hastaFecha = Request.Query["hastaFecha"];
+                    if (desdeFecha != "" && hastaFecha != "")
+                    {
+                        chequesPropiosLista = HelperSQL.GetListaEgresoChequesPropiosEntreFechas(desdeFecha, hastaFecha);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.Default.Error(ExceptionManager.GetExceptionString(ex));
+                }
+                return (chequesPropiosLista.ToArray());
+            }, null, name: "Devuelve la lista cheques propios entre dos fechas dadas. Parámetros: {desdeFecha, hastaFecha}");
+
+            Get<Response>("", p =>
+            {
+                return new Response() { StatusCode = HttpStatusCode.BadRequest };
+            });
+        }
+    }
+}
